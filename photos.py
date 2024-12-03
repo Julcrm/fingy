9730c6cd15f2d4c6caf3099acdf181f02168608c
Julien Castellano <julien@MacBook-Pro-de-Julien.local>
2024-12-03 14:55:09 +0100

    fff

--- photos.py
+++ photos.py
@@ -6,12 +6,12 @@ def photos():
 
     with col1:
         st.markdown("<h3 style='text-align: center; color: white;'>Fingy à la forêt</h3>", unsafe_allow_html=True)
-        st.image("/Leonardo_Phoenix_A_gritty_highcontrast_image_of_a_lone_middle_3.jpg")
+        st.image("Leonardo_Phoenix_A_gritty_highcontrast_image_of_a_lone_middle_3.jpg")
 
     with col2:
         st.markdown("<h3 style='text-align: center; color: white;'>Fingy à la plage</h3>", unsafe_allow_html=True)
-        st.image("/Leonardo_Phoenix_A_solitary_hand_with_a_prominent_middle_finge_0.jpg")
+        st.image("Leonardo_Phoenix_A_solitary_hand_with_a_prominent_middle_finge_0.jpg")
 
     with col3:
         st.markdown("<h3 style='text-align: center; color: white;'>Fingy à la montagne</h3>", unsafe_allow_html=True)
-        st.image("/Leonardo_Phoenix_updatedpromptA_dramatic_highcontrast_image_fe_3.jpg")
+        st.image("Leonardo_Phoenix_updatedpromptA_dramatic_highcontrast_image_fe_3.jpg")
